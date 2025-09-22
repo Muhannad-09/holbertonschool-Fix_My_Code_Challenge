f2153530db3989b5fbc2b0f91193ca851491ae96
Muhannad-09 <muhannadgsgs@gmail.com>
2025-09-22 22:38:40 +0000

    Double linked list

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -3,44 +3,37 @@
 
 /**
  * delete_dnodeint_at_index - Delete a node at a specific index from a list
- * @head: A pointer to the first element of a list
+ * @head: A pointer to the pointer to the first element of a list
  * @index: The index of the node to delete
  *
  * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current;
-    unsigned int i;
+	dlistint_t *current;
+	unsigned int i;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    current = *head;
+	current = *head;
 
-    /* delete head node */
-    if (index == 0)
-    {
-        *head = current->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(current);
-        return (1);
-    }
+	/* move to the node at index */
+	for (i = 0; current != NULL && i < index; i++)
+		current = current->next;
 
-    /* traverse to the index */
-    for (i = 0; current != NULL && i < index; i++)
-        current = current->next;
+	if (current == NULL)
+		return (-1);
 
-    if (current == NULL)
-        return (-1);
+	/* unlink current */
+	if (current->prev)
+		current->prev->next = current->next;
+	else
+		*head = current->next;
 
-    /* unlink current node */
-    if (current->prev)
-        current->prev->next = current->next;
-    if (current->next)
-        current->next->prev = current->prev;
+	if (current->next)
+		current->next->prev = current->prev;
 
-    free(current);
-    return (1);
+	free(current);
+	return (1);
 }
